f093b0f1caf5bfe8a43d2e27169ea5f5baebd783
mannzup4 <94072091+mannzup4@users.noreply.github.com>
2022-09-24 06:44:14 +0530

    Changed api url

--- l4n-task-client/src/features/homepage/dataApi.js
+++ l4n-task-client/src/features/homepage/dataApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const dataApi = createApi({
   reducerPath: "dataApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://l4n-task.herokuapp.com/api" }),
   tagTypes: ["Data"],
   endpoints: (builder) => ({
     getData: builder.query({
--- routes/initRoutes.js
+++ routes/initRoutes.js
@@ -2,5 +2,5 @@ import getRoot from "../controllers/getRoot.js";
 import postRoot from "../controllers/postRoot.js";
 
 export default function initRoutes(app) {
-  app.route("/data").get(getRoot).post(postRoot);
+  app.route("api/data").get(getRoot).post(postRoot);
 }
